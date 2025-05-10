11f102c9e2fa40d95410f111cf034c091950763c
Pedro Luiz <pedrolcavalcanteplcfg.2020@gmail.com>
2025-05-10 10:47:14 -0300

    "resolvendo erros"

--- web-admin/tailwind.config.cjs
+++ web-admin/tailwind.config.cjs
@@ -1,24 +1,10 @@
-// tailwind.config.js
 module.exports = {
-  // 1) aponte pro seu JSX/TSX pra gerar todas as classes que usar
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      // 2) adicione aí as suas famílias de fonte
-      fontFamily: {
-        anton: ['Anton', 'sans‑serif'],
-        'open‑sans': ['Open Sans', 'sans‑serif'],
-        poppins: ['Poppins', 'sans‑serif'],
-      },
-      // se precisar de mais cores (além do verde/ cinza) você pode estender aqui
-      colors: {
-        'verde-principal': '#699530',
-        'cinza-header': '#d9d9d9',
-      },
-    },
+      fontFamily: { anton: ['Anton','sans-serif'], openSans:['Open Sans','sans-serif'], poppins:['Poppins','sans-serif'] },
+      colors: { 'verde-principal':'#699530','cinza-header':'#d9d9d9' },
+    }
   },
   plugins: [],
-}
+};
